e8d2ce53109d60e8970bec6af589a92f53e110d1
Elizabeth Davis <elizabethdavis@stanford.edu>
2026-09-03 01:44:30 -0700

    Add icon + manifest links, version stamp in the build; docs - Home Screen 4/7

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { versionStamp } from './scripts/version-stamp.js'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), versionStamp()],
   build: {
     outDir: 'dist',
     sourcemap: false,
